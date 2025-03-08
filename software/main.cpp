b4d107f9f4ff969b2fd37b7d8912a784e0fc8304
ominjoo <o.minjoo14@gmail.com>
2025-03-07 16:51:26 -0800

    apptracker for MacOS

--- software/main.cpp
+++ software/main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include "mainwindow.h"
+#include "apptracker.h"
 
 
 int main(int argc, char *argv[])
@@ -7,5 +8,14 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    AppTracker appTracker;
+
+    // Connect the signal emitted by AppTracker when the active app changes
+    QObject::connect(&appTracker, &AppTracker::appChanged, [](const QString &appName) {
+        // This lambda function will be called whenever the app changes
+        qDebug() << "Active app changed to:" << appName;
+    });
+
     return a.exec();
 }
